74324521ca9e1584ec6069f2ac96f3951f3ff609
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-05-07 16:37:52 +0300

    update

--- Instagram Clone/scripts/login.js
+++ Instagram Clone/scripts/login.js
@@ -4,71 +4,76 @@ const loginButton = document.getElementById('loginButton');
 
 
 function showPassword() {
-    var x = document.getElementById("passInput");
-    if (x.type === "password") {
-      x.type = "text";
-    } else {
-      x.type = "password";
-    }
+  var x = document.getElementById("passInput");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
   }
+}
 
-// function validateName(){
-//     if(userInput.length < 1){
-//         loginButton.classList.add('non-disable');
-//         return false;
-//     }
-//     else{
-//         return false;
-//     }
-// }
+// document.getElementById('userInput').addEventListener('change',function(){
+//   loginButton.classList.add('non-disable')
+// });
 
 
-(function() {     
-    var i = 0;
-    var pics = [ "./images/login/80b8aebdea57.png", "./images/login/8e9224a71939.png","./images/login/fe2540684ab2.png" ];
-    var el = document.getElementById('imagechange');
+function validateName() {
+  console.log('test')
+  if (userInput.length < 1) {
+    loginButton.classList.add('non-disable');
+  }
+  else {
+    loginButton.classList.remove('non-disable');
+  }
+}
 
-    function toggle() {
-        el.src = pics[i];   
-        i = (i + 1) % pics.length;  
-    }
-    toggle()
-    setInterval(toggle, 1000);
-})();      
+
+(function () {
+  var i = 0;
+  var pics = ["./images/login/80b8aebdea57.png", "./images/login/8e9224a71939.png", "./images/login/fe2540684ab2.png"];
+  var el = document.getElementById('imagechange');
+
+  function toggle() {
+    el.src = pics[i];
+    i = (i + 1) % pics.length;
+  }
+  toggle()
+  setInterval(toggle, 2000);
+})();
 
 
 
 const footerYear = document.getElementById('year');
 
-window.onload = function(){
-    let date = new Date().getFullYear();
-    document.getElementById("year").innerHTML = date;
-    }
+window.onload = function () {
+  let date = new Date().getFullYear();
+  document.getElementById("year").innerHTML = date;
+}
 
 
 
-  loginButton.addEventListener("click",direct)
+loginButton.addEventListener("click", direct)
 
-    function login(){
-      if (userInput =="atalay"&&passInput=="atalay"){
-          direct(),showSuccess();
-      }else{
-        alert("hatalı şifre")
-      }
+function login() {
+  if (userInput == "atalay" && passInput == "atalay") {
+    direct(), showSuccess();
+  } else {
+    alert("hatalı şifre")
   }
-    //Yönlendirme
-    function direct(){
-      window.location.href="./index.html"
-  }
-  
+}
+//Yönlendirme
+function direct() {
+  window.location.href = "./index.html"
+}
 
-  // function errorAlert(){
-  //     errorElement.innerHTML = "Hatalı kullanıcı adı veya şifre"
-  //     errorElement.classList.add("error");
-  // }
-  
 
-loginButton.addEventListener('click',function(){
+// function errorAlert(){
+//     errorElement.innerHTML = "Hatalı kullanıcı adı veya şifre"
+//     errorElement.classList.add("error");
+// }
+
+
+loginButton.addEventListener('click', function () {
   console.log(1)
 })
 
