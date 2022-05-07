7fd644dc16e7dcbd6e67919bf2a3418dbf8f1876
AtalaySezen <atalaysezen@yahoo.com.tr>
2022-05-07 22:21:54 +0300

    instagram story area update!

--- Instagram Clone/scripts/home.js
+++ Instagram Clone/scripts/home.js
@@ -1,28 +1,69 @@
-let togglestatus = true;
-document.addEventListener("DOMContentLoaded", (event) => {
 
-  const p = document.querySelector(".image-profile");
+// let togglestatus = true;
+// document.addEventListener("DOMContentLoaded", (event) => {
 
-  p.addEventListener("click", () => {
-    if (togglestatus === false) {
-      document.querySelector(".drop-down").style.visibility = "hidden";
-      document
-        .querySelector("#home1")
-        .setAttribute(
-          "d",
-          "M45.5 48H30.1c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2.1-4.6-4.6-4.6s-4.6 2.1-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.6-.6 2.1 0l21.5 21.5c.3.3.4.7.4 1.1v23.5c.1.8-.6 1.5-1.4 1.5z"
-        );
+//   const p = document.querySelector(".image-profile");
 
-      togglestatus = true;
-    } else if (togglestatus === true) {
-      document.querySelector(".drop-down").style.visibility = "visible";
-      document
-        .querySelector("#home1")
-        .setAttribute(
-          "d",
-          "M45.3 48H30c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2-4.6-4.6-4.6s-4.6 2-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.5-.6 2.1 0l21.5 21.5c.4.4.6 1.1.3 1.6 0 .1-.1.1-.1.2v22.8c.1.8-.6 1.5-1.4 1.5zm-13.8-3h12.3V23.4L24 3.6l-20 20V45h12.3V34.2c0-4.3 3.3-7.6 7.6-7.6s7.6 3.3 7.6 7.6V45z"
-        );
-      togglestatus = false;
-    }
-  });
-});
+//   p.addEventListener("click", () => {
+//     if (togglestatus === false) {
+//       document.querySelector(".drop-down").style.visibility = "hidden";
+//       document
+//         .querySelector("#home1")
+//         .setAttribute(
+//           "d",
+//           "M45.5 48H30.1c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2.1-4.6-4.6-4.6s-4.6 2.1-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.6-.6 2.1 0l21.5 21.5c.3.3.4.7.4 1.1v23.5c.1.8-.6 1.5-1.4 1.5z"
+//         );
+
+//       togglestatus = true;
+//     } else if (togglestatus === true) {
+//       document.querySelector(".drop-down").style.visibility = "visible";
+//       document
+//         .querySelector("#home1")
+//         .setAttribute(
+//           "d",
+//           "M45.3 48H30c-.8 0-1.5-.7-1.5-1.5V34.2c0-2.6-2-4.6-4.6-4.6s-4.6 2-4.6 4.6v12.3c0 .8-.7 1.5-1.5 1.5H2.5c-.8 0-1.5-.7-1.5-1.5V23c0-.4.2-.8.4-1.1L22.9.4c.6-.6 1.5-.6 2.1 0l21.5 21.5c.4.4.6 1.1.3 1.6 0 .1-.1.1-.1.2v22.8c.1.8-.6 1.5-1.4 1.5zm-13.8-3h12.3V23.4L24 3.6l-20 20V45h12.3V34.2c0-4.3 3.3-7.6 7.6-7.6s7.6 3.3 7.6 7.6V45z"
+//         );
+//       togglestatus = false;
+//     }
+//   });
+// });
+
+
+
+
+let currentScrollPosition = 0;
+let scrollAmount = 320;
+
+const sCont = document.querySelector('.storys-container');
+const hScroll = document.querySelector('.horizontal-scroll');
+
+const btnScrollLeft = document.querySelector('#btn-scroll-left');
+const btnScrollRight = document.querySelector('#btn-scroll-right');
+
+btnScrollLeft.style.opacity ="0";
+
+
+let maxScroll = -sCont.offsetWidth + hScroll.offsetWidth;
+
+function scrollHorizontally(val){
+  currentScrollPosition +=(val*scrollAmount);
+
+  if(currentScrollPosition >0){
+    currentScrollPosition = 0
+    btnScrollLeft.style.opacity ="0";
+  }else{
+    btnScrollLeft.style.opacity ="1";
+  }
+
+  if(currentScrollPosition <maxScroll){
+    currentScrollPosition=maxScroll;
+    btnScrollRight.style.opacity ="0";
+
+  }else{
+    btnScrollRight.style.opacity ="1";
+
+  }
+
+
+  sCont.style.left = currentScrollPosition + "px";
+}
